79414105f96c559fa94070036ad60a1f6cacdfa2
Andrew Berg <andrewberg1215@gmail.com>
2025-11-11 00:12:51 -0800

    Cleaned Up Code
    No function changes were made, but the code has been cleaned up

--- conway.py
+++ conway.py
@@ -1,43 +1,42 @@
 #!/bin/python
-import sys
+
 import random
 import time
 import copy
 
-def neighbors(xinput, yinput):
-    count = 0
-    for xscan in range(-1,2):
-        for yscan in range(-1,2):
-            if xscan != 0 or yscan != 0:
-                    count +=  matrix[int((yinput + yscan) % height)][int((xinput + xscan) % width)]
-    return count
 
-def update():
+def update_matrix():
+    global matrix
     for y in range(height):
         for x in range(width):
-            amount = neighbors(x, y)
-            if amount == 2:
-                if matrix[y][x] == 0:
-                    matrix_buffer[y][x] = 0
-                else:
-                    matrix_buffer[y][x] = 1
-            elif amount == 3:
+            amount = get_neighbors_count(x, y)
+            if amount == 2 and matrix[y][x] == 1 or amount == 3:
                 matrix_buffer[y][x] = 1
             else:
                 matrix_buffer[y][x] = 0
+    matrix = copy.deepcopy(matrix_buffer)
 
-def printmatrix():
-    printbuffer = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
+def get_neighbors_count(x_input, y_input):
+    count = 0
+    for x in range(-1,2):
+        for y in range(-1,2):
+            if matrix[int((y_input + y) % height)][int((x_input + x) % width)] and (x != 0 or y != 0):
+                count += 1
+    return count
+
+
+def render_matrix_terminal():
+    print_buffer = ""
     for y in range(height):
         for x in range(width):
             if matrix[y][x] == 0:
-                printbuffer = printbuffer + " "
-            elif matrix[y][x] == 1:
-                printbuffer = printbuffer + "#"
+                print_buffer = print_buffer + " "
             else:
-                print("ERROR")
-        printbuffer = printbuffer + "\n" 
-    print(printbuffer)
+                print_buffer = print_buffer + "#"
+        print_buffer = print_buffer + "\n" 
+    print(print_buffer)
+
 
 width = 239
 height = 75
@@ -45,8 +44,8 @@ height = 75
 matrix = [[random.randint(0,1) for i in range(width)] for i in range(height)]
 
 matrix_buffer = copy.deepcopy(matrix)
+
 while True:
-    printmatrix()
-    time.sleep(0.1)
-    update()
-    matrix = copy.deepcopy(matrix_buffer)
+    render_matrix_terminal()
+    time.sleep(.1)
+    update_matrix()
